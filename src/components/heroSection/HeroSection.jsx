4d7be4fd85afeed7baf458362fb59f00d9ef89a1
vicky <vikashstd00@gmail.com>
2024-01-11 22:57:16 +0530

    Initial commit

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <div className="w-full h-64">
+      <img
+        className="w-full h-full object-cover"
+        src=" https://media.istockphoto.com/id/1023612090/photo/interior-of-clothing-store.jpg?s=2048x2048&w=is&k=20&c=Z0UG8SlDwrTZqeh3f_jzjcOKW97DL6MzE8uY2vAbYPY="
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default HeroSection;
